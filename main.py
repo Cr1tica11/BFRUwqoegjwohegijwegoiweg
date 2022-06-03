11ff95ed0e4d12a7727fcc95f2a2d201d7bc880a
Critical <105307382+Cr1tica11@users.noreply.github.com>
2022-06-03 18:11:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def deny():
 #So I dont have to have the same stuff twice
 def printhelp():
 	print ("NTP Amplification DOS Attack")
-	print ("By DaRkReD")
+	print ("By ")
 	print ("Usage ntpdos.py <target ip> <ntpserver list> <number of threads>")
 	print ("ex: ex: ntpdos.py 1.2.3.4 file.txt 10")
 	print ("NTP serverlist file should contain one IP per line")
